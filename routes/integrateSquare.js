3d0b1f0710fca68ce61afaf9dc35648c83a1e273
Steven Kwan <stevent.kwan@gmail.com>
2023-11-28 23:58:00 -0800

    Added functionality to receive merchant access_token and refresh_token on application server

--- routes/integrateSquare.js
+++ routes/integrateSquare.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const CryptoJS = require("crypto-js");
+const crypto = require('node:crypto');
+
+function base64URLEncode(str) {
+    return str.toString('base64')
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=/g, '');
+}
+
+function generateCodeVerifier() {
+  return base64URLEncode(crypto.randomBytes(32));
+}
+
+function generateCodeChallenge(code_verifier) {
+  return code_challenge = base64URL(CryptoJS.SHA256(code_verifier))
+}
+function base64URL(string) {
+  return string.toString(CryptoJS.enc.Base64).replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_')
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('integrateSquare');
+  const codeVerifier = generateCodeVerifier();
+  const codeChallenge = generateCodeChallenge(codeVerifier);
+  res.render('integrateSquare', {codeVerifier, codeChallenge});
 });
 
 module.exports = router;
--- routes/oauthRedirect.js
+++ routes/oauthRedirect.js
@@ -4,8 +4,53 @@ const router = express.Router();
 // OrderWeasel Square Sandbox Application ID = 'sandbox-sq0idb-7eLHu6HJWhA_tWlfYSVxXA'
 // OrderWeasel Square Sandbox Application Secret = 'sandbox-sq0csb-D2ld_a1psxQ7a2onFk4yoyfhKWszep-L3SbnafAS8_E'
 
+// the URL returns the `code` parameter which is the `authorization code`. Use this value when you call ObtainToken
+// to get the seller's OAuth access token and refresh token for the seller account. The value you proivde to the ObtainToken call
+// must match this value exactly. If you're using the PKCE flow, you must also include the `code_verifier` in the request to `ObtainToken`
+
+// Note: The `authorization code` expires 5 minutes after the Square authorization page generates the code.
+//       If the code expires before you get an access token, the seller must go to the Square authorization page again to create another authorization code.
+//       The request format to call `ObtainToken` differ between the code flow and the PKCE flow.
+
 router.get('/', (req, res) => {
-  res.json( {msg : 'Hello World'} );
+  console.log(req.body);
+  console.log(req.url);
+  const paramString = req.url.split('?')[1];
+  const nameValuePairs = paramString.split('&');
+  const code = nameValuePairs[0].split('=')[1];
+  console.log(code);
+  const client_id = 'sandbox-sq0idb-7eLHu6HJWhA_tWlfYSVxXA';
+  console.log('WHOOOOOOO HOOOOOOOOOOOOOOOOOOOOOOOOOOOOO!');
+  res.render('squareIntegrationSuccess', {client_id, code});
+});
+
+router.post('/', async (req, res) => {
+  console.log(req.body);
+  const code_verifier = req.body.code_verifier;
+  const code = req.body.code;
+  const client_id = 'sandbox-sq0idb-7eLHu6HJWhA_tWlfYSVxXA';
+  const redirect_uri = 'http://localhost:3000/oauth-redirect';
+  const grant_type = 'authorization_code';
+  //console.log(code_verifier, code, client_id, redirect_uri, grant_type);
+
+  try {
+    const response = await fetch('https://connect.squareupsandbox.com/oauth2/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        code_verifier, code, client_id, redirect_uri, grant_type
+      })
+    });
+
+    if (response.ok) {
+      const result = await response.json();
+      console.log(result);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports =  router;
