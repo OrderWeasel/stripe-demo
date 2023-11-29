c6f3659ccf9fcb292c501f20b4ed4f5f49d21308
Steven Kwan <stevent.kwan@gmail.com>
2023-11-29 01:06:31 -0800

    Added ITEMS_READ, ORDERS_WRITE, ORDERS_READ, and PAYMENTS_WRITE, to merchant authorization request

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'OrderUp' });
+  res.render('index', { title: 'OrderWeasel' });
 });
 
 module.exports = router;
